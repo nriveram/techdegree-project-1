9af15086c26009ef3ebd086787fe2ec6143a6b3f
Naomi <47122756+nriveram@users.noreply.github.com>
2023-09-12 00:10:25 -0700

    project code requirements

--- js/script.js
+++ js/script.js
@@ -10,19 +10,48 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {quote: 'You must always have faith in people. \
+  And most importantly, you must have faith in yourself.', source: 'Elle Woods', citation: 'Legally Blonde', year: '2001'},
+  {quote: 'You know what I still don\'t get? Why are you trying so hard to fit in when \
+  you\'re born to stand out?', source: 'Ian Wallace', citation: 'What a Girl Wants', year: '2003'},
+  {quote: 'They say nothing lasts forever; dreams change, trends come and go, but friendships \
+  never go out of style.', source: 'Carrie Bradshaw', citation: 'Sex and The City', year: '2008'},
+  {quote: 'A queen is never late. Everyone else is simply early.', source: 'Queen Clarisse Renaldi', 
+citation: 'The Princess Diaries', year: '2001'},
+  {quote: 'On October 3rd, he asked me what day it was. “It’s October 3rd.”', source: 'Cady Heron', 
+  citation: 'Mean Girls', year: '2004'},
+  {quote: 'The most exciting, challenging and significant relationship of all is the one you have with yourself. \
+  And if you find someone to love the you you love, well, that\'s just fabulous.', 
+  source: 'Carrie Bradshaw', citation: 'Sex and The City'}
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote() {
+  const randNum = Math.floor(Math.random() * quotes.length); 
+  return quotes[randNum];
+}
 
 
 /***
  * `printQuote` function
 ***/
-
+function printQuote() {
+  let randQuote = getRandomQuote(); 
+  let html = `<p class="quote">${randQuote.quote}</p>
+  <p class="source">${randQuote.source}`; 
+  if (randQuote.citation) {
+    html += `<span class="citation">${randQuote.citation}</span>`;
+  }
+  if (randQuote.year) {
+    html += `<span class="year">${randQuote.year}</span>`; 
+  }
+  html += `</p>`;
+  document.getElementById('quote-box').innerHTML = html; 
+}
 
 
 /***
